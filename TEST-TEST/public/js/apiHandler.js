ee2a87d681ab2758e0fbec249fa097b0f39002cc
johancvb <35685184+johancvb@users.noreply.github.com>
2021-04-16 09:30:14 +0200

    Chat werkt, guesser nog niet.

---
+++ TEST-TEST/public/js/apiHandler.js
@@ -0,0 +1,16 @@
+// const img = document.getElementById('playerImage')
+
+// function fetchPlayers() {
+
+//     fetch(`http://data.nba.net/data/10s/prod/v1/2020/players.json`)
+//     .then(response => response.json())
+//     .then(data => {
+//         const players = data.league.standard;
+//         const randomPlayerID = players[randInt].personId;
+//         const playerImg = `https://ak-static.cms.nba.com/wp-content/uploads/headshots/nba/latest/260x190/${randomPlayerID}.png`;
+
+//         img.src = playerImg
+        
+//     })
+//     return playerImg
+// }
